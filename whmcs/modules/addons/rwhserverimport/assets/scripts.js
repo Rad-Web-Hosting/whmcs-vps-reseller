e9291f19056b6557ed3ea18165c3da1005c4b6a5
Scott Claeys <s.claeys@radwebhosting.com>
2024-03-20 15:27:04 -0500

    Upload rwhserverimport addon files
    Push rwhserverimport Addon module

---
+++ whmcs/modules/addons/rwhserverimport/assets/scripts.js
@@ -0,0 +1,24 @@
+//License : https://bootsnipp.com/snippets/featured/step-wizard-working
+$(document).ready(function() {
+    
+    var navListItems = $('ul.setup-panel li a'),
+        allWells = $('.setup-content');
+
+    allWells.hide();
+
+    navListItems.click(function(e)
+    {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+            $item = $(this).closest('li');
+        
+        if (!$item.hasClass('disabled')) {
+            navListItems.closest('li').removeClass('active');
+            $item.addClass('active');
+            allWells.hide();
+            $target.show();
+        }
+    });
+    
+    $('ul.setup-panel li.active a').trigger('click');      
+});
